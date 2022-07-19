1d552b322d13d86a0b985d47234ab5c1f31f692c
yuzri1984 <yuzrifitriah@hotmail.com>
2022-07-19 14:40:18 +0800

    added cart controller

--- src/main/java/sg/edu/nus/iss/day12workshop/CartController.java
+++ src/main/java/sg/edu/nus/iss/day12workshop/CartController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.ArrayList;
 
 
 
@@ -22,6 +24,28 @@ public class CartController {
 
         return "cart";
     }
+
+    @GetMapping("{itemname}")
+    public String filteredCart(
+    @PathVariable(name="itemname", required= true) String itemname,
+    Model model) {
+    CartService cs = new CartService();
+    List<Item> cartItems = cs.getShoppingItems();
+
+    List<Item> filteredItems = new ArrayList<>();
+    for (Item s : cartItems) {
+        if (s.getItemName().contains(itemname)) {
+            filteredItems.add(s);
+
+        }
+
+    }
+    model.addAttribute("cart", filteredItems);
+    return "cart";    
+
+}
+
+    
 
 
 }
